dc0779cc31689751e7139b936631a26592765ecd
Mohammed Ashhad <76833988+ashhad17@users.noreply.github.com>
2025-03-24 10:53:21 +0530

    Add files via upload

--- src/main/java/com/day10/Airport_management/service/FlightService.java
+++ src/main/java/com/day10/Airport_management/service/FlightService.java
@@ -1,6 +1,7 @@
 package com.day10.Airport_management.service;
 
 
+import com.day10.Airport_management.dto.FlightResponseDTO;
 import com.day10.Airport_management.entity.Flight;
 import com.day10.Airport_management.repository.FlightRepository;
 //package com.airport.flightschedule.service;
@@ -14,18 +15,30 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 
+
+import java.util.stream.Collectors;
+
 @Service
 public class FlightService {
 
     @Autowired
     private FlightRepository flightRepository;
 
-    public List<Flight> getFlights(String source, String destination, LocalDate departureDate) {
-        LocalDateTime startOfDay = departureDate.atStartOfDay(); // 2025-11-17T00:00:00
-        LocalDateTime endOfDay = departureDate.atTime(LocalTime.MAX); // 2025-11-17T23:59:59
+    public List<FlightResponseDTO> getFlights(String source, String destination, LocalDate departureDate) {
+        LocalDateTime startOfDay = departureDate.atStartOfDay();  // 00:00:00
+        LocalDateTime endOfDay = departureDate.atTime(LocalTime.MAX);  // 23:59:59
 
-        return flightRepository.findBySourceAndDestinationAndDepartureDateTimeBetween(
+        List<Flight> flights = flightRepository.findBySourceAndDestinationAndDepartureDateTimeBetween(
                 source, destination, startOfDay, endOfDay);
+
+        return flights.stream().map(flight -> new FlightResponseDTO(
+                flight.getAirline(),
+                flight.getSourceName(),
+                flight.getDestinationName(),
+                flight.getAircraftType(),
+                flight.getDepartureDateTime(),
+                flight.getArrivalDateTime(),
+                flight.getTotalFare()
+        )).collect(Collectors.toList());
     }
 }
-
